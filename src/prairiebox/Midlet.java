722d88717138e324bf7580acc2df552f7bc68135
Benjamin Lamb <bennyfactor@iaatb.net>
2012-10-17 19:57:06 -0400

    midlet file structure

---
+++ src/prairiebox/Midlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prairiebox;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author benlamb
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
