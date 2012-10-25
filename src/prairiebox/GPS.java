2804ac40d2247bf5ed7268b2c8943c8f0b18c964
Benjamin Lamb <bennyfactor@iaatb.net>
2012-10-25 00:38:35 -0400

    Stub for JSR 179 features

---
+++ src/prairiebox/GPS.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prairiebox;
+
+/**
+ *
+ * @author benlamb
+ */
+public class GPS {
+    // http://mobiforge.com/developing/story/developing-location-based-services-introducing-location-api-j2me
+}
